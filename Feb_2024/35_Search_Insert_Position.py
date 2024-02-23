1d9270b3bd02d2a7cd817fb39ca6fddb6839ccd0
daejlee <savif4991@gmail.com>
2024-02-24 01:04:49 +0900

    35 revision

--- Feb_2024/35_Search_Insert_Position.py
+++ Feb_2024/35_Search_Insert_Position.py
@@ -1,3 +1,4 @@
+# TC == O(logn) SC == O(1)
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
         size = len(nums)
@@ -7,7 +8,7 @@ class Solution:
             pivot = size // 2 - 1
         while True:
             size //= 2
-            if size == 0:
+            if size == 0:  # This is post processing
                 if target > nums[pivot]:
                     return pivot + 1
                 elif pivot == 0:
@@ -26,3 +27,25 @@ class Solution:
                     pivot = 0
             else:
                 return pivot
+
+
+# TC == O(logn) SC == O(1) / conventional binary search. using l, r pointer.
+class SolutionV2:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        l = 0
+        r = n - 1
+        while l < r:
+            m = l + (r - l) / 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m
+            else:
+                l = m + 1
+        if nums[l] < target:
+            return l + 1
+        else:
+            return l
