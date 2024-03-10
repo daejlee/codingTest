b36e3bb5f9c9c8423ec70c0e1b373272189838cb
daejlee <savif4991@gmail.com>
2024-03-10 19:51:33 +0900

    Add 392

---
+++ Mar_2024/392_Is_Subsequence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_len = len(s)
+        t_len = len(t)
+        if s_len > t_len:
+            return False
+        i = 0
+        k = 0
+        while i < s_len and k < t_len:
+            if s[i] == t[k]:
+                i += 1
+                k += 1
+            else:
+                k += 1
+        if i == s_len:
+            return True
+        else:
+            return False
+
+
+potato = Solution()
+res = potato.isSubsequence("abc", "adffsbdsgsc")
+print(res)
