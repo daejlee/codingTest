671e8daaa16c14ae26e68173ca0521c69eeca050
daejlee <savif4991@gmail.com>
2024-03-23 03:26:24 +0900

    106 revision

--- Mar_2024/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ Mar_2024/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -6,12 +6,32 @@ class TreeNode:
         self.right = right
 
 
+# TC == O(N^2) SC == (logN)
 class Solution:
     def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
         if not inorder or not postorder:
             return None
         root = TreeNode(postorder.pop())
-        idx = inorder.index(root.val)
+        idx = inorder.index(root.val)  # This one takes O(N) time, NOT COOL!!
         root.right = self.buildTree(inorder[idx + 1 :], postorder)
         root.left = self.buildTree(inorder[:idx], postorder)
         return root
+
+
+# TC == O(N) SC == O(N)
+class SolutionV2:
+    def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
+        map_inorder = {}
+        for i, val in enumerate(inorder):
+            map_inorder[val] = i
+
+        def recur(low, high):
+            if low > high:
+                return None
+            x = TreeNode(postorder.pop())
+            mid = map_inorder[x.val]
+            x.right = recur(mid + 1, high)
+            x.left = recur(low, mid - 1)
+            return x
+
+        return recur(0, len(inorder) - 1)
